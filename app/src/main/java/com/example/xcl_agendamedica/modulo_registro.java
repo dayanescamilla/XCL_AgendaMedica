2f2adcd1561c028afe731682033153077bd9d6cb
Jesus Alquicira B <jesusbernal@gmail.com>
2022-04-07 14:44:41 -0500

    Módulo registro

---
+++ app/src/main/java/com/example/xcl_agendamedica/modulo_registro.java
@@ -0,0 +1,14 @@
+package com.example.xcl_agendamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modulo_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulo_registro);
+    }
+}
