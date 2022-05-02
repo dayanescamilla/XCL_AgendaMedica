1ad0a68d0ee9dc7be5bc2c4563b92443243868a2
Dayan Escamilla <josedayanescamillaperez@gmail.com>
2022-05-02 15:11:15 -0500

    Configuracion boton subir Documentos en Editar_perfil.java

--- app/src/main/java/com/example/xcl_agendamedica/Editar_perfil.java
+++ app/src/main/java/com/example/xcl_agendamedica/Editar_perfil.java
@@ -2,13 +2,29 @@ package com.example.xcl_agendamedica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Editar_perfil extends AppCompatActivity {
+
+    Button subirDoc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+        subirDoc = findViewById(R.id.id_m13_btn1);
+
+        subirDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Editar_perfil.this,Documentos.class);
+                startActivity(i);
+            }
+        });
     }
 }
