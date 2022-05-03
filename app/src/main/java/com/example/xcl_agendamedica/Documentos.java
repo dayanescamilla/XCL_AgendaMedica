e64cd345f40611d471aa57de7a5614ac93fd8837
Dayan Escamilla <josedayanescamillaperez@gmail.com>
2022-05-02 23:45:57 -0500

    Implementacion de la base de datos Firebase y correciones

--- app/src/main/java/com/example/xcl_agendamedica/Documentos.java
+++ app/src/main/java/com/example/xcl_agendamedica/Documentos.java
@@ -3,6 +3,10 @@ package com.example.xcl_agendamedica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Documentos extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class Documentos extends AppCompatActivity {
         setContentView(R.layout.activity_documentos);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //BOTON DE RETROCESO DE ACTION BAR
         this.setTitle("Documentos"); //TITULO MOSTRADO EN ACTION BAR
+    }
+
+
+    public void realtime(View view){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
