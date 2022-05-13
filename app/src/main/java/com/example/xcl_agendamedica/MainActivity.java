af1595017fac4e0c403635601585167c2f8b27e3
Dayan Escamilla <josedayanescamillaperez@gmail.com>
2022-05-12 22:29:50 -0500

    - Correcciones MainActivity.java
    Signed-off-by: Dayan Escamilla <josedayanescamillaperez@gmail.com>

--- app/src/main/java/com/example/xcl_agendamedica/MainActivity.java
+++ app/src/main/java/com/example/xcl_agendamedica/MainActivity.java
@@ -9,17 +9,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //REFERENICAR DATOS
     Button id_m1_btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         setTheme(R.style.Theme_XCL_AgendaMedica);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //LLAMAR A ACTIVITY LOGIN DESDE ACTIVITY MAINACTIVITY
+        //INSTANCIAR DATOS
+
+        //BOTON
         id_m1_btn1 = findViewById(R.id.id_m1_btn1);
+
+        //LLAMAR A ACTIVITY LOGIN DESDE ACTIVITY MAINACTIVITY
         id_m1_btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
