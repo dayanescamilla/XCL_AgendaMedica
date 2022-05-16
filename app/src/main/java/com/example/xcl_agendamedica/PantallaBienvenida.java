c3f13ab5dd220ff47fffc69fd656f49ebf20615c
Dayan Escamilla <josedayanescamillaperez@gmail.com>
2022-05-16 14:04:02 -0500

    - Correcciones y se agregaron diferentes tipos de animacion
    Signed-off-by: Dayan Escamilla <josedayanescamillaperez@gmail.com>

---
+++ app/src/main/java/com/example/xcl_agendamedica/PantallaBienvenida.java
@@ -0,0 +1,39 @@
+package com.example.xcl_agendamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class PantallaBienvenida extends AppCompatActivity {
+
+    LottieAnimationView lottieTime;
+    TextView textoBienvenida, textSub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_bienvenida);
+
+        lottieTime = findViewById(R.id.lottiean);
+        textoBienvenida = findViewById(R.id.txtBienvenido);
+        textSub = findViewById(R.id.textViewSub);
+
+        lottieTime.animate().translationY(0).setDuration(4500).setStartDelay(0);
+        //textoBienvenida.animate().translationX(300).setDuration(3000).setStartDelay(0);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.desvanecidodos, R.anim.desvanecido);
+            }
+        },5000);
+    }
+}
